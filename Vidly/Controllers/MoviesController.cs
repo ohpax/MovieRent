03867457d8eeb6ca4379344b33fbe23b84cc532d
Christ Khodabakhshi <christ.khodabakhshi@gmail.com>
2016-08-18 19:46:56 -0500

    add Movie model.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() {Name = "Shrek!"};
+            //return View(movie);
+            //return Content("Helloooo World !!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sotBy={1}",pageIndex,sortBy));
+        }
+
+        [Route("Movies/Released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content($"{year} / {month}");
+        }
+    }
+}
